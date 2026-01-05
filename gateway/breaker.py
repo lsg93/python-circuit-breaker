a034e0f92ee9ad78b189c0fa8989f4f1bc13043f
Lawrence G <l.s.gwam@gmail.com>
2026-01-05 23:49:26 +0000

    More work

--- gateway/breaker.py
+++ gateway/breaker.py
@@ -1,15 +1,47 @@
+import time
+from dataclasses import dataclass
+from typing import Callable
+
+
+class BreakerCircuitOpenException(Exception):
+    pass
+
+
+@dataclass
 class Breaker:
     """Breaker class - prevents requests to external services from being made if the service is unreliable."""
 
-    def __init__(self, callback):
-        self.callback = callback
-        self.window = []  # This will track the long term progress of requests.
-        self.state = "Closed"  # Breaker state - if false, circuit is 'closed' and working - should be an enum I think.
+    callback: Callable
+    failure_amount: int = 5
+    failure_period: int = 30
+
+    state: str = "Closed"
+    window = []  # which type to use?
 
     def __call__(self):
-        try:
-            if self.state == "Closed":
+        if self.state == "Closed":
+            try:
                 return self.callback()
-        except:
-            # Logic for setting state 'window' ?
-            pass
+            except Exception:
+                self.process_failure()
+        else:
+            raise BreakerCircuitOpenException()
+
+    def check_failures_have_occurred_in_period(self):
+        return True
+
+    def process_failure(self):
+        # Add a timestamp to the array
+        print("here??")
+        self.window.append(time.time())
+        # Look at the count and period properties, and then get the number of timestamps in the array within that threshold
+        if (
+            len(self.window) >= self.failure_amount
+            and self.check_failures_have_occurred_in_period()
+        ):
+            self.trip_breaker()
+            return
+        return
+
+    def trip_breaker(self):
+        self.state = "Open"
--- tests/unit/gateway/breaker_test.py
+++ tests/unit/gateway/breaker_test.py
@@ -1,34 +1,45 @@
-# tests/unit/gateway/breaker_test.py
-
 from unittest.mock import Mock
 
-from gateway.breaker import Breaker
+import pytest
+
+from gateway.breaker import Breaker, BreakerCircuitOpenException
 
 
 def test_breaker_executes_callback_when_circuit_is_closed():
     cb = Mock(return_value=True)
 
-    @Breaker
-    def func():
-        return cb()
-
-    result = func()
+    breaker = Breaker(callback=cb)
+    result = breaker()
 
     cb.assert_called_once()
     assert result
 
 
-# def test_breaker_state_updates_correctly_after_callback_fails():
-#     #todo
+# I think this is a more robust test for the logic than multiple smaller tests which look @ 'private' properties.
+# We immediately 'trip' the breaker by setting its failure threshold to 1
+# Then we try again. It should fail, which proves the state update is handled, and that the logic preventing subsequent requests works.
+def test_breaker_opens_circuit_and_prevents_subsequent_requests_after_failure_threshold_is_met():
+    cb = Mock(return_value=None, side_effect=Exception)
 
-# def test_breaker_prevents_callback_from_being_executed_if_failure_state_is_active():
+    breaker = Breaker(callback=cb, failure_amount=1)
+    breaker()
+    with pytest.raises(BreakerCircuitOpenException):
+        breaker()
+
+    cb.assert_called_once()
+
+
+# def test_breaker_opens_circuit_only_when_failures_occur_in_given_time_period():
+# todo
+
+
+# def test_breaker_independently_checks_if_callback_works_while_failure_state_is_active():
 #     #todo
 
 # def test_breaker_failure_state_is_reset_after_successful_request():
 #     #todo
 
-#     def test_breaker_accepts_failure_threshold_in_pct():
-#     #todo
 
-# def test_breaker_throws_exception_if_callback_result_is_invalid():
+# stretch goal here...
+#     def test_breaker_accepts_failure_threshold_in_pct():
 #     #todo
